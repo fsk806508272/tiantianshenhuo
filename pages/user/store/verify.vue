5dcfb4349221754c1cb908d0e82d7c95137b005b
肖泽楠 <411626080@qq.com>
2020-01-18 18:28:32 +0800

    add vuex module

---
+++ pages/user/store/verify.vue
@@ -0,0 +1,42 @@
+<template>
+	<view>
+		<view class="img">
+			<image src="/static/cut/verify.png"></image>
+		</view>
+		
+		<view class="title">店铺资料审核中</view>
+		<view class="content">审核通过,就可以上传商品哦</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+	.img{
+		text-align: center;
+		margin-top: 175rpx;
+		height:389rpx;
+		image{
+			width:402rpx;
+			height:389rpx;
+		}
+	}
+	
+	.title{
+		width:100%;
+		text-align: center;
+		margin-top: 26rpx;
+		font-size:30rpx;
+		font-weight:400;
+		color:rgba(30,30,30,1);
+	}
+	.content{
+		width:100%;
+		margin-top: 17rpx;
+		text-align: center;
+		font-weight:400;
+		color:rgba(160,160,160,1);
+	}
+	
+</style>
